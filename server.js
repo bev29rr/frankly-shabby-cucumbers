4b1df0be0039a7460cae426f85ffa9be72e3a001
tencalink0 <benca@MSI_DESTROYER>
2025-02-06 22:42:05 +0000

    Working push

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
-import {serveDir} from 'jsr:@std/http/file-server';
-Deno.serve({}, serveDir);
+import { serveDir } from "jsr:@std/http/file-server";
+
+Deno.serve((req, info) => {
+    const url = new URL(req.url);
+    if (url.pathname.startsWith("/static")) {
+        return serveDir(req, { dir: "./static" });
+    } else if (url.pathname === "/requestIP") {
+        const responseData = {
+            ip: info.remoteAddr.hostname,
+        };
+
+        return new Response(JSON.stringify(responseData), {
+            headers: { "Content-Type": "application/json" },
+        });
+    } else  {
+        return serveDir(req, { dir: "./public", fsRoot: "./public"});
+    }
+});
---
+++ static/js/main.js
@@ -0,0 +1,97 @@
+// Come on, don't spoil the fun, go watch it properly first and then come back
+//
+//
+const textInput = document.getElementById("textInput");
+function type(string, speed = 1, callback) {
+    let index = 0;
+    let typing = setInterval(() => {
+        if (string[index] === "*") {
+            textInput.innerHTML += "<br>";
+        } else {
+            textInput.innerHTML += string[index];
+        }
+        index++;
+        if (index >= string.length) {
+            clearInterval(typing);
+            if (callback) callback();
+        }
+    }, speed * 100);
+}
+
+function typeMessages(messages, speed = 1) {
+    if (messages.length === 0) return;
+
+    function typeNextMessage(index) {
+        if (index >= messages.length) return; // breaks recursion
+
+        const currentMessage = messages[index];
+        if (currentMessage === true) {
+            textInput.innerHTML = "";
+            typeNextMessage(index + 1);
+        } else if (!isNaN(currentMessage)) { // detects timeout (for ints)
+            setTimeout(() => {
+                typeNextMessage(index + 1);
+            }, currentMessage * 1000);
+        } else {
+            type(currentMessage, speed, () => {
+                typeNextMessage(index + 1); 
+            });
+        }
+    }
+
+    typeNextMessage(0); // start recursion
+}
+
+function generateDataLog() {
+    return new Promise((resolve, reject) => {
+        let gLocation = "Unknown";
+        fetch('/requestIP')
+            .then(response => response.json())
+            .then(data => {
+                const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+                const platform = /Windows/.test(navigator.userAgent) ? 'Windows' :
+                                 /Macintosh/.test(navigator.userAgent) ? 'Mac' :
+                                 /Linux/.test(navigator.userAgent) ? 'Linux' :
+                                 /Android/.test(navigator.userAgent) ? 'Android' :
+                                 /iPhone|iPad|iPod/.test(navigator.userAgent) ? 'iPhone' :
+                                 'Other';
+
+                const logData = `IP: ${data?.ip || "Unknown"}*Browser Type: ${navigator.userAgent || "Unknown"}*Operating System: ${platform || "Unknown"}*Window Dimensions: x-${screen.width}, y-${screen.height}*IsMobile: ${isMobile}*Geolocation: ${gLocation}`;
+                resolve(logData);
+            })
+            .catch(err => {
+                const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+                const platform = /Windows/.test(navigator.userAgent) ? 'Windows' :
+                                 /Macintosh/.test(navigator.userAgent) ? 'Mac' :
+                                 /Linux/.test(navigator.userAgent) ? 'Linux' :
+                                 /Android/.test(navigator.userAgent) ? 'Android' :
+                                 /iPhone|iPad|iPod/.test(navigator.userAgent) ? 'iPhone' :
+                                 'Other';
+
+                const logData = `<g>IP: Unknown*Browser Type: ${navigator.userAgent || "Unknown"}*Operating System: ${platform || "Unknown"}*Window Dimensions: x-${screen.width}, y-${screen.height}*IsMobile: ${isMobile}*Geolocation: ${gLocation}`;
+                resolve(logData);
+            });
+    });
+}
+
+let dataLog;
+generateDataLog().then(dataLog => {
+    typeMessages(
+        [
+            "You shouldn't have ventured this far", 
+            3, true,
+            "There are no Frankly Shabby Cucumbers here", 
+            1, true,
+            "It's just darkness", 
+            0.5, true,
+            "Pure darkness",
+            0.02, ". ", 0.02, ". ", 0.02, ". ",
+            0.7, true, 4,
+            "Want to see something?",
+            2, true,
+            "I wasn't asking by the way",
+            2, true, 1.5,
+            dataLog
+        ], 0.3
+    );
+})
